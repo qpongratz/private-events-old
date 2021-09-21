8b7a355dc56207c25192783c9fff40e76927e754
qpongratz <lagarathan@gmail.com>
2021-09-21 13:09:17 -0500

    Create basic event create form with tie in to controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,20 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).expect(:date, :name, :place)
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User',
                        inverse_of: :created_events
+  validates :name, presence: true
+  validates :date, presence: true
+  validates :place, presence: true
 end
